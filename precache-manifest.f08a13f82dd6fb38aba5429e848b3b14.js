c8f15bfbedea97675e907eb634ac139aa167c19b
deepakTelus <deepak.nagarkoti@telusinternational.com>
2020-09-28 01:45:55 +0530

    Updates

---
+++ precache-manifest.f08a13f82dd6fb38aba5429e848b3b14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbdee3b6c4266460f8adbfe682916c8",
+    "url": "/spacex-overview/index.html"
+  },
+  {
+    "revision": "b1912a1b4965ab3469d7",
+    "url": "/spacex-overview/static/css/main.24f835d2.chunk.css"
+  },
+  {
+    "revision": "b07110442b43d1748269",
+    "url": "/spacex-overview/static/js/2.c8ed67f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spacex-overview/static/js/2.c8ed67f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1912a1b4965ab3469d7",
+    "url": "/spacex-overview/static/js/main.c40898ab.chunk.js"
+  },
+  {
+    "revision": "2e72b481ef258f8d8b87",
+    "url": "/spacex-overview/static/js/runtime-main.045eda76.js"
+  }
+]);
